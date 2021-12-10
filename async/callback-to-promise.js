6f07f31ad91c7cdda3f857a6c87f600255a750da
RadiantK <pps8853@gmail.com>
2021-12-10 12:48:14 +0900

    콜백 복습

--- async/callback-to-promise.js
+++ async/callback-to-promise.js
@@ -1,38 +1,58 @@
+// // Callback Hell example
 // class UserStorage {
 //   loginUser(id, password) {
-//     return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         if (
-//           (id === 'ellie' && password === 'dream') ||
-//           (id === 'coder' && password === 'academy')
-//         ) {
-//           resolve(id);
-//         } else {
-//           reject(new Error('not found'));
-//         }
-//       }, 1000)
-//     })
-//   }
+//     return new Promise((resolve,reject) => {
+//     setTimeout(() => {
+//       if (
+//         (id === 'kang' && password === 'asdf') ||
+//         (id === 'coder' && password === 'academy')
+//       ) {
+//         resolve(id);
+//       } else {
+//         reject(new Error('not found'));
+//       };
+//     }, 2000);
+//   });
+//  };
+
 //   getRoles(user) {
 //     return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         if (user === 'ellie') {
-//           resolve({name: 'ellie', role: 'admin'});
-//         } else {
-//           reject(new Error('no access'));
-//         }   
-//       }, 1000);
-//     });
-//   }
-// }
+//     setTimeout(() => {
+//       if (user === 'kang') {
+//         resolve({name: 'kang', role: 'admin'});
+//       } else {
+//         reject(new Error('no access'));
+//       } 
+//     }, 1000);
+//    });
+//   };
+// };
 
 // const userStorage = new UserStorage();
 // const id = prompt('enter your id');
 // const password = prompt('enter your password');
 // userStorage.loginUser(id, password)
 // .then(userStorage.getRoles)
-// .then(user => alert(`Hello ${user.name}, you have a ${user.role} role`))
-// .catch(console.log)
+// .then(user => alert(`Hello ${user.name}, you have a ${user.role} role`));
+
+
+// userStorage.loginUser(id, password ,user => {
+//     userStorage.getRoles(user, userWithRole => {
+//         alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
+//       },
+//       error => {
+//         console.log(error);
+//       }
+//     );
+//   },
+//   error => {
+//     console.log(error);
+//   }
+// )
+
+
+
+
 
 
 // // 연습
@@ -121,45 +141,45 @@
 
 // ================================
 
-class UserStorage {
-  loginUser(id, password) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if(
-          (id === 'kang' && password === 'asdf') ||
-          (id === 'kook' && password === 'asdf')) {
-           resolve(id) 
-          } else {
-            reject(new Error('ERROR!!'))
-          }
-      }, 2000)
-    })
-  }
-  getRoles(user) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (user === 'kang') {
-          resolve({name: 'kang', role: 'admin'})
-        } else {
-          reject(new Error('no access'))
-        }
-      }, 1000)
-    })
+// class UserStorage {
+//   loginUser(id, password) {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         if(
+//           (id === 'kang' && password === 'asdf') ||
+//           (id === 'kook' && password === 'asdf')) {
+//            resolve(id) 
+//           } else {
+//             reject(new Error('ERROR!!'))
+//           }
+//       }, 2000)
+//     })
+//   }
+//   getRoles(user) {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         if (user === 'kang') {
+//           resolve({name: 'kang', role: 'admin'})
+//         } else {
+//           reject(new Error('no access'))
+//         }
+//       }, 1000)
+//     })
    
-  }
-}
+//   }
+// }
 
-const userStorage = new UserStorage();
-const id = prompt('login your id')
-const password = prompt('login your password')
-async function order() {
-  try {
-  const result1 = await userStorage.loginUser(id, password);
-  const user = await userStorage.getRoles(result1)
-  return `Hello ${user.name} your Roles ${user.role}.`
-  } catch(err) {
-    console.log(err)
-  }
-}
+// const userStorage = new UserStorage();
+// const id = prompt('login your id')
+// const password = prompt('login your password')
+// async function order() {
+//   try {
+//   const result1 = await userStorage.loginUser(id, password);
+//   const user = await userStorage.getRoles(result1)
+//   return `Hello ${user.name} your Roles ${user.role}.`
+//   } catch(err) {
+//     console.log(err)
+//   }
+// }
 
-order().then(console.log)
+// order().then(console.log)
